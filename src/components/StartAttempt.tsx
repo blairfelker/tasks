09e1c4f114e174d2bd76ab7b3db06d9f77a03610
Blair <blairf@udel.edu>
2022-09-22 01:14:36 -0400

    StartAttempt.tsx Solved

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [value, setValue] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+    function startHelper(): void {
+        setValue(value - 1);
+        setProgress(!inProgress);
+    }
+    return (
+        <span>
+            <Button
+                onClick={() => startHelper()}
+                disabled={inProgress || value <= 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => setProgress(!inProgress)}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+            <Button onClick={() => setValue(value + 1)} disabled={inProgress}>
+                Mulligan
+            </Button>
+            Attempts: {value}
+        </span>
+    );
 }
