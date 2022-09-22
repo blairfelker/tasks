03a816150e18a2217844e8d257a46edcb7066a95
Blair <blairf@udel.edu>
2022-09-22 13:31:23 -0400

    ChangeType.tsx Solved, removed HTML and text tests as per instructions.

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,28 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, changeQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeType() {
+        if (questionType === "multiple_choice_question") {
+            changeQuestionType("short_answer_question");
+        } else {
+            changeQuestionType("multiple_choice_question");
+        }
+    }
+
+    return (
+        <div>
+            <span>
+                {questionType === "short_answer_question" && (
+                    <div>Short Answer</div>
+                )}
+                {questionType === "multiple_choice_question" && (
+                    <div>Multiple Choice</div>
+                )}
+                <Button onClick={() => changeType()}>Change Type</Button>
+            </span>
+        </div>
+    );
 }
