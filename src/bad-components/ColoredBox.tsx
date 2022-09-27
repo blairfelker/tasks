8dc2e68baa43441fb86daeaa6cd64e7dc8fd2060
Blair <blairf@udel.edu>
2022-09-27 17:38:40 -0400

    Solved ColoredBox.tsx

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,23 +4,22 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+interface colorInterface {
+    helper: () => void;
 }
 
-function ColorPreview(): JSX.Element {
+function ChangeColor({ helper }: colorInterface): JSX.Element {
+    return <Button onClick={helper}>Next Color</Button>;
+}
+
+function ColorPreview({ index }: { index: number }): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[index],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,13 +29,15 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+    const changeIndex = () => setColorIndex((1 + colorIndex) % COLORS.length);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor helper={changeIndex}></ChangeColor>
+                <ColorPreview index={colorIndex}></ColorPreview>
             </div>
         </div>
     );
