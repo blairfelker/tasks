01a3f065a51ca43defc61ec146c5878400b090bc
Blair <blairf@udel.edu>
2022-10-13 11:28:00 -0400

    not done

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("");
+    //Red, Orange, Yellow, Green, Cyan, Blue, Purple, Magenta
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {"You have chosen" + color}
         </div>
     );
 }
